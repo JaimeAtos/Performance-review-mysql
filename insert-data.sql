46a3411e9c7f3a5d1822b47633035b6b414b22b1
Jaime Fernandez <luis-jaime.fernandez@atos.net>
2022-07-18 09:13:34 -0500

    Documentos faltantes

--- insert-data.sql
+++ insert-data.sql
@@ -1,12 +1,12 @@
 USE Performance_review;
 
-insert into User (Name, Birthday) values
-	('Daniel Sebastiao', '1984-07-24'),
-    ('Edwin Julian', '1991-12-25'),
-    ('Mariano Pacienza', '1987-05-30'),
-    ('Saulo Villasenor', '1977-11-02'),
-    ('Hector Cisnero', '1975-01-17'),
-    ('Horeb Gastelum', '1993-08-14')
+insert into User (Name, Birthday, CreateDate) values
+	('Daniel Sebastiao', '1984-07-24',CURDATE()),
+    ('Edwin Julian', '1991-12-25',CURDATE()),
+    ('Mariano Pacienza', '1987-05-30',CURDATE()),
+    ('Saulo Villasenor', '1977-11-02',CURDATE()),
+    ('Hector Cisnero', '1975-01-17',CURDATE()),
+    ('Horeb Gastelum', '1993-08-14',CURDATE())
 ;
 
 insert into Role (ProfileName) values
